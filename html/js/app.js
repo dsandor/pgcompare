a551c49b5709463278da0db0a8f36a621d10d0a9
David Sandor <david.sandor@gmail.com>
2015-09-07 17:35:18 -0500

    Initial commit.

---
+++ html/js/app.js
@@ -0,0 +1,126 @@
+var ipc = require('ipc');
+var remote = require('remote');
+var dialog = remote.require('dialog');
+
+angular.module('pgCompareApp', [])
+    .controller('mainController', function() {
+        var todoList = this;
+        todoList.todos = [
+            {text:'learn angular', done:true},
+            {text:'build an angular app', done:false}];
+
+        todoList.addTodo = function() {
+            todoList.todos.push({text:todoList.todoText, done:false});
+            todoList.todoText = '';
+        };
+
+        todoList.remaining = function() {
+            var count = 0;
+            angular.forEach(todoList.todos, function(todo) {
+                count += todo.done ? 0 : 1;
+            });
+            return count;
+        };
+
+        todoList.archive = function() {
+            var oldTodos = todoList.todos;
+            todoList.todos = [];
+            angular.forEach(oldTodos, function(todo) {
+                if (!todo.done) todoList.todos.push(todo);
+            });
+        };
+    })
+
+    .controller('settingsController',['$scope','$http', function($scope, $http) {
+        $scope.sourceSettings = {};
+        $scope.destinationSettings = {};
+        $scope.isSaving = false;
+
+        $scope.startCompare = function() {
+            ipc.send('start-compare', $scope.sourceSettings, $scope.destinationSettings);
+        };
+
+        $scope.saveSettings = function() {
+            dialog.showSaveDialog(function (filename) {
+                if (filename === undefined) return;
+
+                var settings = { sourceSettings: $scope.sourceSettings, destinationSettings: $scope.destinationSettings };
+
+                $scope.isSaving = true;
+                ipc.send('save-settings', filename, settings);
+            });
+        };
+
+        ipc.on('saved-settings', function(message) {
+            $scope.$apply(function() {
+                $scope.isSaving = false;
+            });
+        });
+
+        ipc.on('load-settings', function(settings) {
+            $scope.$apply(function() {
+                $scope.sourceSettings = settings.sourceSettings;
+                $scope.destinationSettings = settings.destinationSettings;
+            });
+        });
+
+    }])
+
+    .controller('changesController',['$scope', function($scope) {
+
+        $scope.sourceTables = [ ];
+        $scope.isLoading = true;
+        $scope.compareResults = [ ];
+        $scope.hideMatches = true;
+
+        // TODO: make this controller get the settings from the main process.
+
+        $scope.loadingMessage = "Loading tables...";
+
+        $scope.criteriaMatch = function( criteria ) {
+            return function( item ) {
+                if (!$scope.hideMatches) return true;
+
+                return item.status != '=';
+            };
+        };
+
+        $scope.hideMatchesClick = function() {
+            $scope.hideMatches = !$scope.hideMatches;
+            console.log('hideMatches? ' + $scope.hideMatches);
+        };
+
+        $scope.applyChanges = function() {
+            console.log('applyChanges called, ' + $scope.compareResults.length);
+            for(var i= 0,max=$scope.compareResults.length; i<max; i++)
+            {
+                if ($scope.compareResults[i].isSelected) {
+                    console.log('selected change: ' + JSON.stringify($scope.compareResults[i]));
+                }
+            }
+        };
+
+        startCompare = function() {
+            ipc.send('apply-changes', $scope.sourceSettings, $scope.destinationSettings);
+        };
+
+        //comparing-message
+        ipc.on('comparing-message', function(msg) {
+            $scope.$apply(function() {
+                $scope.loadingMessage = msg;
+            });
+        });
+
+        ipc.on('schema-data-loaded', function(schemaData) {
+
+            $scope.$apply(function() {
+                $scope.isLoading = false;
+
+                $scope.sourceTables = schemaData.sourceSchema;
+                $scope.destinationTables = schemaData.destinationSchema;
+                $scope.compareResults = schemaData.compareResults;
+            });
+
+        });
+
+    }]);
---
+++ index.js
@@ -0,0 +1,408 @@
+var app = require('app'),  // Module to control application life.
+    BrowserWindow = require('browser-window'),  // Module to create native browser window.
+    ipc = require('ipc'),
+    pg = require('pg'),
+    _ = require('lodash'),
+    fs = require('fs'),
+    async = require('async');
+
+// Report crashes to our server.
+require('crash-reporter').start();
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is GCed.
+var mainWindow = null;
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function() {
+    // On OS X it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform != 'darwin') {
+        app.quit();
+    }
+});
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+app.on('ready', function() {
+    // Create the browser window.
+    mainWindow = new BrowserWindow({width: 800, height: 620});
+
+    // and load the index.html of the app.
+    mainWindow.loadUrl('file://' + __dirname + '/html/index.html');
+
+    // Open the devtools.
+    //mainWindow.openDevTools();
+
+    // Emitted when the window is closed.
+    mainWindow.on('closed', function() {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        mainWindow = null;
+    });
+});
+
+ipc.on('main-log', function(event, arg) {
+    console.log('main-log', 'arg:', arg);
+    event.returnValue = 'Thanks from main-log.';
+});
+
+ipc.on('start-compare', function(event, source, dest) {
+    startCompare(source, dest);
+});
+
+// TODO: need to make the renderer side send the filename and then here
+// load it which gets the source and dest configs.  these are then loaded and sent to
+// settings screen.
+ipc.on('navigate-load-settings', function(event, filename) {
+
+    console.log('nls: ' + filename[0]);
+
+    // somehow push the settings to the settings screen.
+    mainWindow.setTitle('Settings');
+    mainWindow.loadUrl('file://' + __dirname + '/html/settings.html');
+
+    fs.readFile(filename[0], function(err, data) {
+        console.log('loaded file: ' + filename[0]);
+        console.log('loaded data: ' + data);
+
+    setTimeout(function() {
+            console.log('about to call load-settings...');
+            mainWindow.webContents.send('load-settings', JSON.parse(data));
+        }, 650);
+    });
+});
+
+ipc.on('navigate-settings', function() {
+    mainWindow.setTitle('Settings');
+    mainWindow.loadUrl('file://' + __dirname + '/html/settings.html');
+
+});
+
+ipc.on('save-settings', function(event, filename, settings) {
+
+    if (filename.indexOf('.pgc') < 1) filename += '.pgc';
+
+    fs.writeFile(filename, JSON.stringify(settings), function(err) {
+        mainWindow.webContents.send('settings-saved', filename);
+    });
+});
+
+function startCompare(source, dest) {
+    console.log('source', source, 'dest', dest);
+
+    mainWindow.setTitle('Comparing ' + source.name + ' to ' + dest.name + '...');
+    mainWindow.loadUrl('file://' + __dirname + '/html/compare.html');
+
+    var results = {
+        sourceSchema: {},
+        destinationSchema: {}
+    };
+
+    resolveObjects(source, dest, function(r) {
+        results = r;
+
+        compareRoutines(results, function(routineCompareResults) {
+            console.log('called: schema-data-loaded');
+            mainWindow.webContents.send('schema-data-loaded', routineCompareResults);
+        });
+    })
+}
+
+function resolveObjects(source, dest, callback) {
+    var results = {
+        sourceSchema: {},
+        destinationSchema: {}
+    };
+
+    var completedCount = 0, totalCount = 3;
+
+    getSchemaData(source, function(err, source_result) {
+        console.log('got source schema data');
+
+        results.sourceSchema = source_result;
+
+        getSchemaData(dest, function(err, dest_result) {
+            console.log('got dest schema data');
+            results.destinationSchema = dest_result;
+
+            compareSchemas(results, function(compareResults) {
+                results.compareResults = compareResults;
+
+                console.log('compareResults:', JSON.stringify(compareResults));
+
+                completedCount++;
+
+                if (completedCount == totalCount) {
+                    callback(results);
+                }
+            });
+        });
+    });
+
+    getRoutineData(source, function(err, result) {
+        results.sourceRoutines = result;
+
+        completedCount++;
+
+        if (completedCount == totalCount) {
+            callback(results);
+        }
+    });
+
+    getRoutineData(dest, function(err, result) {
+        results.destinationRoutines = result;
+        completedCount++;
+
+        if (completedCount == totalCount) {
+            callback(results);
+        }
+    });
+}
+
+function compareRoutines(schemaData, cb) {
+
+    for(var i= 0, max= schemaData.sourceRoutines.length; i < max; i++) {
+        //console.log('routine: ' + schemaData.sourceRoutines[i].routine_name + '\r\nddl: ' + schemaData.sourceRoutines[i].ddl);
+        var destRoutine = _.findWhere(schemaData.destinationRoutines, { routine_name: schemaData.sourceRoutines[i].routine_name });
+
+        if (typeof destRoutine == 'undefined') {
+            schemaData.compareResults.push(
+                {
+                    object_type: 'routine',
+                    source_object_name: schemaData.sourceRoutines[i].routine_name,
+                    destination_object_name: '',
+                    status: '!>',
+                    isSelected: false
+                }
+            );
+        } else {
+            if (schemaData.sourceRoutines[i].ddl == destRoutine.ddl) {
+                schemaData.compareResults.push(
+                    {
+                        object_type: 'routine',
+                        source_object_name: schemaData.sourceRoutines[i].routine_name,
+                        destination_object_name: destRoutine.routine_name,
+                        status: '=',
+                        isSelected: false
+                    }
+                );
+            } else {
+                schemaData.compareResults.push(
+                    {
+                        object_type: 'routine',
+                        source_object_name: schemaData.sourceRoutines[i].routine_name,
+                        destination_object_name: destRoutine.routine_name,
+                        status: '!=',
+                        isSelected: false
+                    }
+                );
+            }
+        }
+    }
+
+    cb(schemaData);
+}
+
+function compareSchemas(schemaData, cb) {
+
+    console.log('source table count: ' + schemaData.sourceSchema.length +
+    ' dest table count: ' + schemaData.destinationSchema.length);
+
+    var compareResults = _.map(schemaData.sourceSchema, function(sourceItem) {
+
+        mainWindow.webContents.send('comparing-message', 'comparing table: ' + sourceItem.table_name);
+
+        console.log('sourceItem.columns: ' + sourceItem.columns.length);
+
+        var destMatch = _.findWhere(schemaData.destinationSchema, { 'table_name': sourceItem.table_name  });
+
+        if (typeof destMatch == 'undefined') {
+            return {
+                object_type: 'table',
+                source_object_name: sourceItem.table_name,
+                destination_object_name: '',
+                status: '!>',
+                isSelected: false
+            };
+        }
+
+        return {
+            object_type: 'table',
+            source_object_name: sourceItem.table_name,
+            destination_object_name: destMatch.table_name,
+            status: '=',
+            isSelected: false
+        };
+    });
+
+    for(var i= 0, max=schemaData.destinationSchema.length; i < max; i++) {
+        var sourceMatch = _.findWhere(schemaData.sourceSchema, { 'table_name': schemaData.destinationSchema[i].table_name  });
+
+        if (typeof sourceMatch == 'undefined') {
+            compareResults.push({
+                object_type: 'table',
+                source_object_name: '',
+                destination_object_name: schemaData.destinationSchema[i].table_name,
+                status: '!<',
+                isSelected: false
+            });
+        }
+    }
+
+
+    cb(compareResults);
+}
+
+// http://www.postgresql.org/docs/current/interactive/infoschema-routines.html
+function getSchemaData(serverData, cb) {
+    var conString = "postgres://" + serverData.server + "/" + serverData.database;
+
+    console.log('connection string:', conString);
+
+    pg.defaults.port = serverData.port;
+    pg.defaults.ssl = true;
+    pg.defaults.user = serverData.username;
+    pg.defaults.password = serverData.password;
+    pg.defaults.host = serverData.server;
+    pg.defaults.database = serverData.database;
+
+    mainWindow.webContents.send('comparing-message', 'loading schema: ' + serverData.name);
+
+    pg.connect(conString, function(err, client, done) {
+        if(err) {
+            cb(err);
+
+            return console.error('error fetching client from pool', err);
+        }
+
+        client.query(sql_table_list, [serverData.schema], function(err, result) {
+            //call `done()` to release the client back to the pool
+            done();
+
+            if(err) {
+                cb(err);
+
+                return console.error('error running query', err);
+            }
+
+            // Gets all the columns for the table.
+            async.eachSeries(result.rows, function(item, async_done) {
+                    client.query(sql_table_details, [serverData.schema, item.table_name], function(col_err, col_result) {
+                        done();
+
+                        if(col_err) {
+                            async_done(col_err);
+
+                            return console.error('error running query', col_err);
+                        }
+                        item.columns = col_result.rows;
+
+                        async_done();
+                    });
+            },
+            function() {
+                cb(undefined, result.rows);
+            });
+
+        });
+    });
+}
+
+function getRoutineData(serverData, cb) {
+    var conString = "postgres://" + serverData.server + "/" + serverData.database;
+
+    console.log('connection string:', conString);
+
+    pg.defaults.port = serverData.port;
+    pg.defaults.ssl = true;
+    pg.defaults.user = serverData.username;
+    pg.defaults.password = serverData.password;
+    pg.defaults.host = serverData.server;
+    pg.defaults.database = serverData.database;
+
+    mainWindow.webContents.send('comparing-message', 'loading routines: ' + serverData.name);
+
+    pg.connect(conString, function(err, client, done) {
+        if(err) {
+            cb(err);
+
+            return console.error('error fetching client from pool', err);
+        }
+
+        client.query(sql_routine_list, [serverData.schema], function(err, result) {
+            done();
+
+            if(err) {
+                cb(err);
+
+                return console.error('error running query', err);
+            }
+
+
+            // Gets all the DDL for the routines.
+            async.eachSeries(result.rows, function(item, async_done) {
+                    mainWindow.webContents.send('comparing-message', 'getting ddl for: ' + item.routine_name);
+
+                    var specificNameElements = item.specific_name.split('_');
+                    var routineId = specificNameElements[specificNameElements.length-1];
+
+                        client.query(sql_routine_ddl, [routineId], function(col_err, col_result) {
+                        done();
+
+                        if(col_err) {
+                            async_done(col_err);
+
+                            return console.error('error running query', col_err);
+                        }
+                        item.ddl = col_result.rows[0].pg_get_functiondef;
+
+                            //console.log('item.ddl: ' + item.ddl);
+                        async_done();
+                    });
+                },
+                function() {
+                    cb(undefined, result.rows);
+                });
+
+        });
+    });
+}
+/*
+pg.connect(conString, function(err, client, done) {
+    if(err) {
+        return console.error('error fetching client from pool', err);
+    }
+    client.query('SELECT $1::int AS number', ['1'], function(err, result) {
+        //call `done()` to release the client back to the pool
+        done();
+
+        if(err) {
+            return console.error('error running query', err);
+        }
+        console.log(result.rows[0].number);
+        //output: 1
+    });
+});
+*/
+
+// $1 = schema name
+// $2 = table name
+var sql_table_details = "select c.table_catalog, c.table_schema, c.table_name, c.column_name, " +
+"c.column_default, c.is_nullable, c.data_type, c.character_maximum_length, " +
+"    c.numeric_precision, c.numeric_precision_radix, c.numeric_scale, c.datetime_precision, " +
+"    c.udt_name " +
+"from " +
+"information_schema.columns c " +
+"where table_schema = $1 AND table_name = $2 " +
+"order by ordinal_position ASC;";
+
+// $1 = schema name
+var sql_table_list = "select * from information_schema.tables where table_schema = $1;";
+
+// $1 = schema name
+var sql_routine_list = "select * FROM information_schema.routines where routine_schema = $1;";
+
+// $1 = routine id
+var sql_routine_ddl = "select * from pg_get_functiondef($1);";
